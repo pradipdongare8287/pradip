cb7eb9bef912c5c989a3b528f123f9ecfcb1ca04
pradipdongare8287 <pradipshankardongare@gmail.com>
2026-08-19 22:58:57 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 const util = require('util');
 
 const database = mysql.createConnection({
-    host:'bvuxs385ugidqk9hjd3q-mysql.services.clever-cloud.com',
-    user:'ul8vlqf525asrbqf',
-    password:'Ur114AXv23Rofp2jyTyF',
-    database:'bvuxs385ugidqk9hjd3q'
+    host:'bkgwrn6gtcty6cdwl5o2-mysql.services.clever-cloud.com',
+    user:'u2egwb6gtty7oe3d',
+    password:'To6M35rfToqNQJe9dtUm',
+    database:'bkgwrn6gtcty6cdwl5o2'
 })
 
 const query = util.promisify(database.query).bind(database);
