bd02a4692a0b5700cc56370d0d0868d550100ec3
pradipdongare8287 <pradipshankardongare@gmail.com>
2026-08-20 07:18:13 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql2');
 const util = require('util');
 
-const database = mysql.createConnection({
+const database = mysql.createPool({
     host:'bkgwrn6gtcty6cdwl5o2-mysql.services.clever-cloud.com',
     user:'u2egwb6gtty7oe3d',
+    port: '3306',
     password:'To6M35rfToqNQJe9dtUm',
     database:'bkgwrn6gtcty6cdwl5o2'
 })
